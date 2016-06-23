4b196f41c5cfdbc7643cc5e87346ade6f3c38da2
Pokechu22 <Pokechu022@gmail.com>
2016-06-23 16:50:08 -0700

    Add particle effects for redstone to obsidian

--- src/com/wasteofplastic/askyblock/listeners/RedstoneToObsidian.java
+++ src/com/wasteofplastic/askyblock/listeners/RedstoneToObsidian.java
@@ -16,7 +16,13 @@
  *******************************************************************************/
 package com.wasteofplastic.askyblock.listeners;
 
+import java.util.Random;
+
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
@@ -33,6 +39,7 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class RedstoneToObsidian implements Listener {
 	private final JavaPlugin plugin;
+	private Random rand = new Random();
 	public RedstoneToObsidian(JavaPlugin plugin) {
 		this.plugin = plugin;
 	}
@@ -60,8 +67,31 @@ public class RedstoneToObsidian implements Listener {
 				if (wouldMakeCobble) {
 					toBlock.setType(Material.OBSIDIAN);
 					e.setCancelled(true);
+					
+					playMixEffect(toBlock.getLocation());
 				}
 			}
+		}
+	}
+	
+	private void playMixEffect(Location location) {
+		World world = location.getWorld();
+		
+		world.playSound(location, Sound.BLOCK_LAVA_EXTINGUISH, .5f,
+				2.6f + (rand.nextFloat() - rand.nextFloat()) * 0.8f);
+		
+		for (int i = 0; i < 8; i++) {
+			// Normal smoke
+			world.spawnParticle(Particle.SMOKE_LARGE,
+					location.getX() + Math.random(), location.getY() + 1.2,
+					location.getZ() + Math.random(), 1, 0, 0, 0, 0, null);
+			// Bonus purple and red particles
+			world.spawnParticle(Particle.PORTAL,
+					location.getX() + Math.random(), location.getY() + 1.2,
+					location.getZ() + Math.random(), 1, 0, 0, 0, 0, null);
+			world.spawnParticle(Particle.REDSTONE,
+					location.getX() + Math.random(), location.getY() + 1.2,
+					location.getZ() + Math.random(), 1, 0, 0, 0, 0, null);
 		}
 	}
 }
